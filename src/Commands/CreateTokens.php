37f3e12af14d307210b73d9a286aeef71ed8a872
Michael Ramirez <mramirez@ole.cr>
2022-12-20 23:31:17 -0600

    Creacion de tokens

---
+++ src/Commands/CreateTokens.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace MichaelRamirezApi\Commands;
+
+use Exception;
+use MichaelRamirezApi\Utils\Config;
+use MichaelRamirezApi\Utils\SQLiteConnection;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class CreateTokens extends Command
+{
+    /**
+     * The name of the command (the part after "bin/demo").
+     *
+     * @var string
+     */
+    protected static $defaultName = 'create:tokens';
+
+    /**
+     * The command description shown when running "php bin/demo list".
+     *
+     * @var string
+     */
+    protected static $defaultDescription = 'Create tokens to the application';
+
+    /**
+     * Execute the command
+     *
+     * @param  InputInterface  $input
+     * @param  OutputInterface $output
+     * @return int 0 if everything went fine, or an exit code.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $config = Config::create();
+        $pdo = SQLiteConnection::create();
+        $dbConnetion = $pdo->connect($config->getDataBasePath());
+        $io = new SymfonyStyle($input, $output);
+        try {
+            $envFile = file_get_contents($config->getRootDir() . '/.env');
+            $envFile = str_replace("API_TOKEN='", "API_TOKEN='" . md5(microtime()), $envFile);
+            $envFile = str_replace("API_TOKEN_READ='", "API_TOKEN_READ='" . md5(microtime()), $envFile);
+            file_put_contents($config->getRootDir() . '/.env', $envFile);
+            
+            $io->success('Tokens generated!');
+        }catch(Exception $ex){
+            $io->error(sprintf('Error: %s', $ex->getMessage()));
+        }finally{
+            //Close DB connection 
+            $pdo->close();
+        }
+
+        return Command::SUCCESS;
+    }
+}
